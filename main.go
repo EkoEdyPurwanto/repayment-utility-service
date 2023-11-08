561c4f4730355548f56966d47ab3b0c1ea26fbf7
EkoEdyPurwanto <ekoedypurwantooke@gmail.com>
2023-11-08 21:57:33 +0700

    [FIX] content of main.go

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("repayment-utility-service App V1")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "repayment-utility-service App V1")
+	})
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
